7fb02b807ec5a139a804a0a9b8c1b2c57bcc3652
evilghostgirl <julia.anna.glaszka@interia.pl>
2019-03-05 17:50:31 +0100

    pierwszy

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'visualize'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
